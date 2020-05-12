4b4a1d2177a980759e00d2cf96eb8a311cff6724
Abdel-Raouf <raoufmohamed@outlook.com>
2020-05-13 00:28:40 +0200

    implemented cd command relative tokens (..), to get back in a directory hierarchy

--- src/com/scala/commands/Cd.scala
+++ src/com/scala/commands/Cd.scala
@@ -10,8 +10,9 @@ class Cd(dir: String) extends Command {
   override def apply(state: State): State = {
     /*
       cd /something/somethingElse/.../  = absolute path
-      cd a/b/c = relative to the current working directory
+      cd a/b/c = relative to the current working directory (relative path)
 
+      // relative tokens
       cd ..
       cd .
       cd a/./.././a/
@@ -48,10 +49,66 @@ class Cd(dir: String) extends Command {
         else findEntryHelper(nextDir.asDirectory, path.tail)
       }
 
+    @tailrec
+    def collapseRelativeTokens(path: List[String], result: List[String]): List[String] = {
+      /*
+        /a/b => ["a", "b"]
+
+        path.isEmpty? => no
+          CRT(["b"], result = List :+ "a" = ["a"])
+            path.isEmpty? => no
+              CRT([], result = LIST :+ "b" = ["a", "b"])
+                path.isEmpty? => yes
+                  result
+
+        /a/.. => ["a", ".."]
+
+        path.isEmpty? => no
+          CRT([".."], result = [] :+ "a" = ["a"])
+             path.isEmpty? => no
+              CRT([], []) = []
+
+        /a/b/.. => ["a", "b", ".."]
+
+        path.isEmpty? => no
+          CRT(["b", ".."], result = [] :+ "a" = ["a"])
+            path.isEmpty? => no
+              CRT([".."], result = ["a"] :+ ".." = ["a", "b"])
+                path.isEmpty()? => no
+                  CRT([], "a")
+
+        /a/b/c/.. => ["a", "b", "c", ".."]
+        etc...
+
+      */
+
+      if(path.isEmpty) result
+      else if(".".equals(path.head)) collapseRelativeTokens(path.tail, result)
+      else if("..".equals(path.head)) {
+        if(result.isEmpty) null
+        else collapseRelativeTokens(path.tail, result.init)
+      } else collapseRelativeTokens(path.tail, result :+ path.head)
+    }
+
     // 1- tokens
     val tokens: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
 
+    // 1.5 - eliminate/collapse relative tokens
+
+    /*
+
+      ["a", "."] => ["a"]
+      ["a", "b", ".", "."] => ["a", "b"]
+
+      /a/../ => ["a", ".."] => []
+      /a/b/.. => ["a", "b", ".."] => ["a"]
+
+    */
+
+    val newTokens = collapseRelativeTokens(tokens, List())
+
     // 2- navigate to the correct entry
-    findEntryHelper(root, tokens)
+    if(newTokens == null) null
+    else findEntryHelper(root, newTokens)
   }
 }
